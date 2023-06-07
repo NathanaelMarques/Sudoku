12d7467b9b52c3b378e5181ae985573552ce23f8
nathanael.rocha <nathanael.rocha@partnerbank.com.br>
2023-06-07 17:03:58 -0300

    Lógica de geração de tabuleiro implementada

--- src/TabuleiroSudoku.java
+++ src/TabuleiroSudoku.java
@@ -7,73 +7,89 @@ public class TabuleiroSudoku {
 
     public TabuleiroSudoku(){}
 
+
+    // GERADOR DO TABULEIRO(FALTANDO IMPLEMENTAR LÓGICA PARA GERAR NÚMEROS CORRETAMENTE)
     public TabuleiroSudoku[][] geraTab(TabuleiroSudoku[][] tabuleiro){
         Random random = new Random();
-
+        int numRandom;
         for(int c = 0; c < tabuleiro.length; c++) {
             for (int z = 0; z < tabuleiro.length; z++) {
                 tabuleiro[c][z] = new TabuleiroSudoku();
                 for (int x = 0; x < quadrante.length; x++) {
                     for (int y = 0; y < quadrante[x].length; y++) {
+                        tabuleiro[c][z].quadrante[x][y] = 0;
+                        numRandom = random.nextInt(10);
+                        if(comparador(tabuleiro,x,y,numRandom,c,z)){
+                            tabuleiro[c][z].quadrante[x][y] = numRandom;
+                        }
 
-                        tabuleiro[c][z].quadrante[x][y] = random.nextInt(10);
                     }
                 }
             }
         }
         return tabuleiro;
     }
-    private boolean verificaLinha(TabuleiroSudoku[][] tabuleiro, int linha, int input,int coordenadasX){
+
+    // VERIFICADORES
+    private boolean verificaLinha(TabuleiroSudoku[][] tabuleiro, int linha, Integer input,int coordenadasX){
 
             for(int x = 0; x < tabuleiro.length; x++){
-                for(int y = 0; y < tabuleiro[coordenadasX].length; y++){
-                        if (tabuleiro[coordenadasX][x].quadrante[linha][y] == input) {
+                if(tabuleiro[coordenadasX][x]!=null) {
+                    for (int y = 0; y < tabuleiro[coordenadasX].length; y++) {
+                        if (input.equals(tabuleiro[coordenadasX][x].quadrante[linha][y])) {
                             return false;
                         }
+                    }
                 }
             }
             return true;
     }
 
-    private boolean verificaColuna(TabuleiroSudoku[][] tabuleiro, int coluna, int input, int coordenadasY){
+    private boolean verificaColuna(TabuleiroSudoku[][] tabuleiro, int coluna, Integer input, int coordenadasY){
         for(int x = 0; x < tabuleiro.length; x++){
-            for(int y = 0; y < tabuleiro.length; y++){
-                    if (tabuleiro[x][coordenadasY].quadrante[y][coluna] == input) {
+            if (tabuleiro[x][coordenadasY] != null) {
+                for (int y = 0; y < tabuleiro.length; y++) {
+                    if (input.equals(tabuleiro[x][coordenadasY].quadrante[y][coluna])) {
                         return false;
                     }
+                }
             }
         }
         return true;
     }
 
-    private boolean verificaQuadrante(TabuleiroSudoku[][] tabuleiro, int coordenadasX, int coordenadasY, int input){
+    private boolean verificaQuadrante(TabuleiroSudoku[][] tabuleiro, int coordenadasX, int coordenadasY, Integer input){
         for(int x = 0; x < tabuleiro.length; x++){
             for(int y = 0; y < tabuleiro[coordenadasX].length; y++){
-                    if (tabuleiro[coordenadasX][coordenadasY].quadrante[x][y] == input) {
-                        return false;
-                    }
+                if(input.equals(tabuleiro[coordenadasX][coordenadasY].quadrante[x][y])) {
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    public boolean comparador(TabuleiroSudoku[][] tabuleiro,int linha, int coluna, int input,
+    public boolean comparador(TabuleiroSudoku[][] tabuleiro,int linha, int coluna, Integer input,
                               int coordenadasX, int coordenadasY){
         if(verificaColuna(tabuleiro, coluna, input,coordenadasY)
            &&
            verificaLinha(tabuleiro, linha, input,coordenadasX)
            &&
            verificaQuadrante(tabuleiro,coordenadasX, coordenadasY, input)
-           &&input>0&&input<10){
+           &&input!=null){
             return true;
         }
         return false;
     }
+
+    // ADICIONA NOVOS ELEMENTOS AO TABULEIRO
     public TabuleiroSudoku[][] addTabuleiro(TabuleiroSudoku[][] tabuleiro,int coordenadasX, int coordenadasY,
                                             int linha, int coluna, int input){
         tabuleiro[coordenadasX][coordenadasY].quadrante[linha][coluna] = input;
         return tabuleiro;
     }
+
+    // IMPRIME O TABULEIRO FORMATADO
     public void verTabuleiro(TabuleiroSudoku[][] tabuleiro){ //lógica insana de print
         for(int x =0; x<tabuleiro.length;x++){
             for(int y = 0;y< tabuleiro[x].length;y++){
@@ -81,7 +97,7 @@ public class TabuleiroSudoku {
                     for(int c = 0; c < tabuleiro[x][y].quadrante[z].length; c++){
                          if(tabuleiro[x][z].quadrante[y][c]!=0){
                              System.out.print("|" + tabuleiro[x][z].quadrante[y][c] + "|");
-                         }else{
+                         }else {
                              System.out.print("|" + " |");
                          }
 
